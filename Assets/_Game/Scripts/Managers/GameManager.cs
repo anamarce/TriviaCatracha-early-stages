be4bf148739028aab84b76bd8604d25d9155dc15
Rogger Vasquez <rogger.vasquez@grupoleitz.com>
2014-05-27 11:36:15 -0600

    Agregado el game play services , agregue Parse, Hice Refactor de algunas clases y pasaron al namespace x16. , renombre las carpetas nuestras con guion bajo y aparezcan de primero *** NO mover las carpetas de Facebook y Google Play a la carpeta plugins, dejarlas en raiz de Assets

---
+++ Assets/_Game/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+
+
+    public  class GameManager : MonoBehaviour {
+	
+	
+
+	
+	
+        void Start()
+        {
+     
+            Time.timeScale = 1.0f;
+      
+     
+        }
+
+   
+	
+	
+    }
