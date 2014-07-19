3bfd6ce55c52e76cffe09f6fba83fa74fa70bbff
Rogger Vasquez <rogger.vasquez@grupoleitz.com>
2014-07-18 22:30:07 -0600

    Iniciando con el cloudmanager y game progress

---
+++ Assets/_Game/Scripts/TurnBased/GameProgress.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameProgress  {
+
+	private const string PlayerPrefsKey = "triviageek-game-progress";
+
+   // do we have modifications to write to disk/cloud?
+    private bool mDirty = false;
+
+    public GameProgress()
+    {
+    }
+
+}
